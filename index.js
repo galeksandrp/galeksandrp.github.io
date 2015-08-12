898da42f76fa4f91711824d036e1e622d79fcda0
Alexander Georgievskiy <galeksandrp@gmail.com>
2015-08-12 20:45:38 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var yandex = new JSO({
+    client_id: '9b24befae37e42678a83d84af48a0e61',
+    authorization: 'https://oauth.yandex.ru/authorize'
+});
+yandex.callback();
+
+$('#yandex').click(function (e) {
+    e.preventDefault();
+    yandex.getToken();
+});
